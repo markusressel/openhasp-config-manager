56985953a34f76d41597e66fcd6392098c44e2b1
Markus Ressel <mail@markusressel.de>
2023-02-18 19:38:49 +0100

    added first cmd validation

--- openhasp_config_manager/validation/cmd.py
+++ openhasp_config_manager/validation/cmd.py
@@ -3,10 +3,18 @@ from openhasp_config_manager.validation import Validator
 
 class CmdFileValidator(Validator):
 
-    def validate(self, content: str):
-        for line in content.splitlines():
+    def validate(self, data: str):
+        for line in data.splitlines():
             line = line.lstrip()
             self._validate_line(line)
 
-    def _validate_line(self, line):
-        pass
+    def _validate_line(self, line: str):
+        if line.startswith("run"):
+            pass
+        elif line.startswith("jsonl"):
+            command, arg = line.split(sep=" ", maxsplit=1)
+            try:
+                import json
+                json.loads(arg)
+            except Exception as ex:
+                raise AssertionError(f"jsonl command argument cannot be parsed: {ex}")
---
+++ tests/validation/cmd_validator_test.py
@@ -0,0 +1,38 @@
+import textwrap
+
+from openhasp_config_manager.validation.cmd import CmdFileValidator
+from tests import TestBase
+
+
+class TestCmdFileValidator(TestBase):
+
+    def test_single_jsonl_command_valid(self):
+        # GIVEN
+        under_test = CmdFileValidator()
+
+        data = textwrap.dedent("""
+            jsonl {"obj":"btn","id":14,"x":120,"y":1,"w":30,"h":40,"text_font":"2","text":"Test","text_color":"gray","bg_opa":0,"border_width":0}
+        """).strip()
+
+        # WHEN
+        under_test.validate(data)
+
+        # THEN
+        assert True
+
+    def test_single_jsonl_command_invalid(self):
+        # GIVEN
+        under_test = CmdFileValidator()
+
+        data = textwrap.dedent("""
+           jsonl {"obj":"btn","id":14,"x":120,"y":1,"w":30,"h":40,"text_font":"2","text":"Test","text_color":"gray","bg_opa":0,"border_width":0
+        """).strip()
+
+        try:
+            # WHEN
+            under_test.validate(data=data)
+            assert False
+        except Exception as ex:
+            # THEN
+            assert str(
+                ex) == "jsonl command argument cannot be parsed: Expecting ',' delimiter: line 1 column 127 (char 126)"
