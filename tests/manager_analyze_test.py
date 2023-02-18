79dc61fe3b1e021cb4ae3eb740e995f059c3e8bf
Markus Ressel <mail@markusressel.de>
2023-02-18 17:25:18 +0100

    moved test

--- tests/manager_analyze_test.py
+++ tests/manager_analyze_test.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 from openhasp_config_manager.manager import ConfigManager
 from openhasp_config_manager.processing import VariableManager
 from tests import TestBase
@@ -21,20 +19,4 @@ class TestConfigManager(TestBase):
         components = devices[0].components
         assert len(components) == 6
 
-    def test_global_variable(self, tmp_path):
-        # GIVEN
-        variable_manager = VariableManager(self.cfg_root)
-        manager = ConfigManager(self.cfg_root, tmp_path, variable_manager)
-        devices = manager.analyze()
 
-        # WHEN
-        tested_component_path = Path(self.cfg_root, "test_device", "home_page.jsonl")
-        result = variable_manager.get_vars(tested_component_path)
-
-        # THEN
-        assert result == {
-            "global": {
-                "var": "global_var_value"
-            },
-            "key_also_present_in_device_vars": "global_value"
-        }
--- tests/variable_manager_test.py
+++ tests/variable_manager_test.py
@@ -1,19 +1,23 @@
+from pathlib import Path
+
 from openhasp_config_manager.processing import VariableManager
 from tests import TestBase
 
 
 class TestVariableManager(TestBase):
 
-    def test_get_variables_for_pathanalyze_whole_config(self, tmp_path):
+    def test_global_variable(self, tmp_path):
         # GIVEN
-        # TODO: write
-
         variable_manager = VariableManager(self.cfg_root)
+        tested_component_path = Path(self.cfg_root, "test_device", "home_page.jsonl")
+
         # WHEN
-        devices = variable_manager.get_vars()
+        result = variable_manager.get_vars(tested_component_path)
 
         # THEN
-        assert len(devices) == 1
-
-        components = devices[0].components
-        assert len(components) == 6
+        assert result == {
+            "global": {
+                "var": "global_var_value"
+            },
+            "key_also_present_in_device_vars": "global_value"
+        }
