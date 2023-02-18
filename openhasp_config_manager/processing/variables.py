207e2587f05b02196132e37089ae556cc04bc5a0
Markus Ressel <mail@markusressel.de>
2023-02-18 17:57:56 +0100

    refactoring

--- openhasp_config_manager/processing/variables.py
+++ openhasp_config_manager/processing/variables.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Dict
+from typing import Dict, Any
 
 import yaml
 from yaml import Loader
@@ -14,9 +14,9 @@ class VariableManager:
 
     def __init__(self, cfg_root: Path):
         self._cfg_root = Path(cfg_root)
-        self._read()
+        self._path_vars = self._read(cfg_root)
 
-    def get_vars(self, path: Path) -> Dict:
+    def get_vars(self, path: Path) -> [str, Any]:
         """
         Returns the variable definitions and values for a given path.
 
@@ -30,7 +30,17 @@ class VariableManager:
         path_vars = self._get_vars_for_path(path)
         return path_vars
 
-    def _get_vars_for_path(self, path: Path):
+    def _get_vars_for_path(self, path: Path) -> Dict[str, Any]:
+        """
+        Returns the variable definitions and values for a given path.
+
+        The resulting map will be a combination of all variables available within each
+        of the directories along the given path. Variable definitions found in a lower (longer) path
+        will override the ones that might be present higher up the path.
+
+        :param path: the path context to use for variable evaluation
+        :return: a map of "variable name" -> "variable value given the path context"
+        """
         result = {}
         relative_paths = path.relative_to(self._cfg_root).parent.parts
 
@@ -45,27 +55,34 @@ class VariableManager:
 
         return result
 
-    def _read(self):
-        for toplevel_path in [self._cfg_root]:
-            for path in toplevel_path.glob('**/**'):
-                if not path.is_dir():
+    def _read(self, path: Path) -> Dict[str, Dict]:
+        """
+        Reads all "*.yaml" variable definition files in the given path
+        :return: a map of "path -> dict of variables"
+        """
+        result = {}
+        for toplevel_path in [path]:
+            for p in toplevel_path.glob('**/**'):
+                if not p.is_dir():
                     continue
 
-                if path.name.startswith("."):
+                if p.name.startswith("."):
                     continue
 
-                path_var_files = path.glob("*.yaml")
+                path_var_files = p.glob("*.yaml")
                 for file in path_var_files:
                     if not file.is_file():
                         continue
 
                     data = self._load_var_file(file)
 
-                    sub_path = path.relative_to(self._cfg_root)
+                    sub_path = p.relative_to(path)
                     sub_path_str = str(sub_path)
-                    if sub_path_str not in self._path_vars.keys():
-                        self._path_vars[sub_path_str] = {}
-                    self._path_vars[sub_path_str] |= data
+                    if sub_path_str not in result.keys():
+                        result[sub_path_str] = {}
+                    result[sub_path_str] |= data
+
+        return result
 
     @staticmethod
     def _load_var_file(file: Path) -> Dict:
