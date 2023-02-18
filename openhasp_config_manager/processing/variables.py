c0f74327575a2b81c32679c04ae43346b930973b
Markus Ressel <mail@markusressel.de>
2023-02-18 16:16:47 +0100

    docs and slight refactoring

--- openhasp_config_manager/processing/variables.py
+++ openhasp_config_manager/processing/variables.py
@@ -6,13 +6,26 @@ from yaml import Loader
 
 
 class VariableManager:
-    path_vars = {}
+    """
+    Used to manage variable definitions found in YAML files inside the configuration
+    directory.
+
+    Variable values are resolved by inversely traversing the file path (from bottom to top).
+    The first variable definition found will be used.
+    """
+    _path_vars = {}
 
     def __init__(self, cfg_root: Path):
         self._cfg_root = Path(cfg_root)
         self._read()
 
     def get_vars(self, path: Path) -> Dict:
+        """
+        Returns the variable definitions and values for a given path.
+
+        :param path: the path context to use for variable evaluation
+        :return: a map of "variable name" -> "variable value given the path context"
+        """
         path_vars = self._get_vars_for_path(path)
         return path_vars
 
@@ -23,11 +36,11 @@ class VariableManager:
         current_path = self._cfg_root.relative_to(self._cfg_root)
 
         current_path_str = str(current_path)
-        result |= self.path_vars.get(current_path_str, {})
+        result |= self._path_vars.get(current_path_str, {})
         for subfolder in relative_paths:
             current_path = Path(current_path, subfolder)
             current_path_str = str(current_path)
-            result |= self.path_vars.get(current_path_str, {})
+            result |= self._path_vars.get(current_path_str, {})
 
         return result
 
@@ -49,11 +62,11 @@ class VariableManager:
 
                     sub_path = path.relative_to(self._cfg_root)
                     sub_path_str = str(sub_path)
-                    if sub_path_str not in self.path_vars.keys():
-                        self.path_vars[sub_path_str] = {}
-                    self.path_vars[sub_path_str] |= data
+                    if sub_path_str not in self._path_vars.keys():
+                        self._path_vars[sub_path_str] = {}
+                    self._path_vars[sub_path_str] |= data
 
     @staticmethod
-    def _load_var_file(file: Path):
+    def _load_var_file(file: Path) -> Dict:
         content = file.read_text()
         return yaml.load(content, Loader=Loader)
