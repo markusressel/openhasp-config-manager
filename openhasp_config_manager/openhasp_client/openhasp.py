2f3f90569f44519e36d238c407d02278f30b3056
Markus Ressel <mail@markusressel.de>
2023-04-05 20:34:18 +0200

    added asyncio-mqtt, refactoring of webservice related methods into WebserviceClient class, reworked MqttClient for asyncio-mqtt

--- openhasp_config_manager/openhasp_client/openhasp.py
+++ openhasp_config_manager/openhasp_client/openhasp.py
@@ -1,8 +1,5 @@
 import json
-from typing import Dict, List, Any, Callable, Union
-
-import orjson
-import requests as requests
+from typing import Dict, List, Any, Callable
 
 from openhasp_config_manager.openhasp_client.model.device import Device
 from openhasp_config_manager.openhasp_client.model.gui_config import GuiConfig
@@ -10,11 +7,8 @@ from openhasp_config_manager.openhasp_client.model.hasp_config import HaspConfig
 from openhasp_config_manager.openhasp_client.model.http_config import HttpConfig
 from openhasp_config_manager.openhasp_client.model.mqtt_config import MqttConfig
 from openhasp_config_manager.openhasp_client.mqtt_client import MqttClient
+from openhasp_config_manager.openhasp_client.webservice_client import WebserviceClient
 from openhasp_config_manager.ui.util import info
-
-GET = "GET"
-POST = "POST"
-DELETE = "DELETE"
 
 
 class OpenHaspClient:
@@ -24,7 +18,12 @@ class OpenHaspClient:
         :param device: the device this client can communicate with
         """
         self._device = device
-        self._base_url = self._compute_base_url(self._device)
+
+        self._webservice_client = WebserviceClient(
+            url=device.config.openhasp_config_manager.device.ip,
+            username=self._device.config.http.user,
+            password=self._device.config.http.password
+        )
 
         self._mqtt_client = MqttClient(
             host=device.config.mqtt.host,
@@ -33,7 +32,7 @@ class OpenHaspClient:
             mqtt_password=device.config.mqtt.password
         )
 
-    def send_image(self, image: any, page: int, object_id: int):
+    async def send_image(self, image: any, page: int, object_id: int):
         """
         Send an image to the device
         :param image: the image to send
@@ -42,43 +41,43 @@ class OpenHaspClient:
         """
         pass
 
-    def set_text(self, obj: str, text: str):
+    async def set_text(self, obj: str, text: str):
         """
         Set the text of an object
         :param obj: the object to set the text for
         :param text: the text to set
         """
-        self.set_object_properties(obj, {"text": text})
+        await self.set_object_properties(obj, {"text": text})
 
-    def set_object_properties(self, obj: str, properties: Dict[str, Any]):
+    async def set_object_properties(self, obj: str, properties: Dict[str, Any]):
         """
         Set the state of a plate
         :param obj: object to set a state for
         :param properties: properties to set
         """
         for prop, value in properties.items():
-            self.command(
+            await self.command(
                 name=f"{obj}.{prop}",
                 params=value,
             )
 
-    def set_idle_state(self, state: str):
+    async def set_idle_state(self, state: str):
         """
         Forces the given idle state
         :param state: one of "off", "short", "long"
         """
-        return self.command(
+        return await self.command(
             name="idle",
             params=state
         )
 
-    def set_backlight(self, state: bool, brightness: int):
+    async def set_backlight(self, state: bool, brightness: int):
         """
         Sets the backlight state and brightness
         :param state: True to turn on, False to turn off
         :param brightness: the brightness level in 0..255
         """
-        return self.command(
+        return await self.command(
             name="backlight",
             params=json.dumps({
                 "state": state,
@@ -86,92 +85,41 @@ class OpenHaspClient:
             })
         )
 
-    def wakeup(self):
+    async def wakeup(self):
         """
         Wakes up the display
         """
-        return self.set_idle_state(state="off")
+        return await self.set_idle_state(state="off")
 
-    def set_page(self, index: int):
+    async def set_page(self, index: int):
         """
         Set the current page
         :param index: the index of the page to set
         """
-        return self.command(
+        return await self.command(
             name="page",
             params=f"{index}"
         )
 
-    def set_hidden(self, obj: str, hidden: bool):
+    async def set_hidden(self, obj: str, hidden: bool):
         """
         Set the "hidden" property of an object
         :param obj: the object to set the property for
         :param hidden: True to hide the object, False to show it
         """
-        return self.set_object_properties(
+        return await self.set_object_properties(
             obj=obj,
             properties={
                 "hidden": "1" if hidden else "0",
             }
         )
 
-    def get_files(self) -> List[str]:
-        """
-        Retrieve a list of all file on the device
-        :return: a list of all files on the device
-        """
-        username = self._device.config.http.user
-        password = self._device.config.http.password
-
-        response = self._do_request(
-            method=GET,
-            url=self._base_url + "list?dir=/",
-            username=username, password=password
-        )
-        response_data = orjson.loads(response.decode('utf-8'))
-
-        files = list(filter(lambda x: x["type"] == "file", response_data))
-        file_names = list(map(lambda x: x["name"], files))
-        return file_names
-
-    def get_file_content(self, file_name: str) -> str or None:
-        username = self._device.config.http.user
-        password = self._device.config.http.password
-
-        try:
-            response = self._do_request(
-                method=GET,
-                url=self._base_url + file_name,
-                username=username, password=password
-            )
-            response_data = response.decode('utf-8')
-            return response_data
-        except Exception as ex:
-            return None
-
-    def delete_file(self, file_name: str):
-        """
-        Delete a file on the device
-        :param file_name: the name of the file
-        """
-        username = self._device.config.http.user
-        password = self._device.config.http.password
-
-        self._do_request(
-            method=DELETE,
-            url=self._base_url + "edit",
-            data={
-                "path": "/" + file_name
-            },
-            username=username, password=password
-        )
-
-    def listen_state(
+    async def listen_state(
             self,
             callback: Callable,
             state: str,
             plate: str = "+",
-    ) -> Union[str, list]:
+    ):
         """
         Listen to OpenHASP state events
         :param callback: callback to call when a matching event is received
@@ -192,12 +140,12 @@ class OpenHaspClient:
             if (plate == "+" or event_plate == plate) and event_state_name == state:
                 await callback(event_topic, event_plate, event_state_name, event_payload)
 
-        self.listen_event(
+        await self.listen_event(
             path=f"hasp/{plate}/state/{state}",
             callback=_callback,
         )
 
-    def listen_event(self, path: str, callback: Callable):
+    async def listen_event(self, path: str, callback: Callable):
         """
         Listen for an event
         :param path:
@@ -209,9 +157,9 @@ class OpenHaspClient:
             callback(message)
 
         topic = f"hasp/{self._device.config.mqtt.name}/{path}"
-        self._mqtt_client.subscribe(topic, _on_message)
+        await self._mqtt_client.subscribe(topic, _on_message)
 
-    def command(self, name: str, params: str):
+    async def command(self, name: str, params: str):
         """
         Execute a command on a device
         :param name: the name of the command
@@ -219,91 +167,47 @@ class OpenHaspClient:
         """
         topic = f"hasp/{self._device.config.mqtt.name}/command/{name}"
         data = params.strip('"')
-        self._mqtt_client.publish(topic=topic, payload=data)
+        await self._mqtt_client.publish(topic=topic, payload=data)
+
+    def get_files(self) -> List[str]:
+        """
+        Retrieve a list of all file on the device
+        :return: a list of all files on the device
+        """
+        return self._webservice_client.get_files()
+
+    def get_file_content(self, file_name: str) -> str or None:
+        return self._webservice_client.get_file_content(file_name)
+
+    def delete_file(self, file_name: str):
+        """
+        Delete a file on the device
+        :param file_name: the name of the file
+        """
+        self._webservice_client.delete_file(file_name)
 
     def reboot(self):
         """
         Request a reboot
         """
-        username = self._device.config.http.user
-        password = self._device.config.http.password
-        self._do_request(
-            method=GET,
-            url=self._base_url + "reboot",
-            username=username, password=password
-        )
+        self._webservice_client.reboot()
 
     def set_hasp_config(self, config: HaspConfig):
         """
         Set the "HASP" configuration
         :param config: the configuration to set
         """
-        data = {
-            "startpage": config.startpage,
-            "startdim": config.startdim,
-            "theme": config.theme,
-            "color1": config.color1,
-            "color2": config.color2,
-            "font": config.font,
-            "pages": config.pages,
-            "save": "hasp"
-        }
-
-        # ignore keys with None value
-        data = {k: v for k, v in data.items() if v is not None}
-
-        username = self._device.config.http.user
-        password = self._device.config.http.password
-
-        self._do_request(
-            method=POST,
-            url=self._base_url + "config",
-            data=data,
-            username=username, password=password
-        )
+        self._webservice_client.set_hasp_config(config)
 
     def set_http_config(self, config: HttpConfig):
         """
         Set the HTTP configuration
         :param config: the configuration to set
         """
-        data = {
-            "user": config.user,
-            "pass": config.password,
-            "save": "http"
-        }
-
-        # ignore keys with None value
-        data = {k: v for k, v in data.items() if v is not None}
-
-        username = self._device.config.http.user
-        password = self._device.config.http.password
-
-        self._do_request(
-            method=POST,
-            url=self._base_url + "config",
-            data=data,
-            username=username, password=password
-        )
+        self._webservice_client.set_http_config(config)
 
     def get_mqtt_config(self) -> MqttConfig:
-        username = self._device.config.http.user
-        password = self._device.config.http.password
-
-        data = self._do_request(
-            method=GET,
-            url=self._base_url + "api/config/mqtt/",
-            username=username, password=password
-        )
-
-        return MqttConfig(
-            name=data["name"],
-            group=data["group"],
-            host=data["host"],
-            port=data["port"],
-            user=data["user"],
-            password=data["pass"]
-        )
+        return self._webservice_client.get_mqtt_config()
 
     def set_mqtt_config(self, config: MqttConfig):
         """
@@ -311,28 +215,7 @@ class OpenHaspClient:
         :param config: the configuration to set
         :return:
         """
-        data = {
-            "name": config.name,
-            "group": config.group,
-            "host": config.host,
-            "port": config.port,
-            "user": config.user,
-            "pass": config.password,
-            "save": "mqtt"
-        }
-
-        # ignore keys with None value
-        data = {k: v for k, v in data.items() if v is not None}
-
-        username = self._device.config.http.user
-        password = self._device.config.http.password
-
-        self._do_request(
-            method=POST,
-            url=self._base_url + "config",
-            data=data,
-            username=username, password=password
-        )
+        self._webservice_client.set_mqtt_config(config)
 
     def set_gui_config(self, config: GuiConfig):
         """
@@ -340,35 +223,14 @@ class OpenHaspClient:
         :param config: the configuration to set
         :return:
         """
-        data = {
-            "idle1": config.idle1,
-            "idle2": config.idle2,
-            "rotate": config.rotate,
-            "cursor": config.cursor,
-            "bckl": config.bckl,
-            "save": "gui"
-        }
-
-        # ignore keys with None value
-        data = {k: v for k, v in data.items() if v is not None}
-
-        username = self._device.config.http.user
-        password = self._device.config.http.password
-
-        self._do_request(
-            method=POST,
-            url=self._base_url + "config",
-            data=data,
-            username=username, password=password
-        )
+        self._webservice_client.set_gui_config(config)
 
     def upload_files(self, files: Dict[str, str]):
         """
         Upload a collection of files
         :param files: "target file name"->"file content" mapping
         """
-        for name, content in files.items():
-            self.upload_file(name, content)
+        self._webservice_client.upload_files(files)
 
     def upload_file(self, name: str, content: str):
         """
@@ -377,79 +239,11 @@ class OpenHaspClient:
         :param content: the file content
         """
         info(f"Uploading '{name}'...")
-
-        username = self._device.config.http.user
-        password = self._device.config.http.password
-        self._do_request(
-            method=POST,
-            url=self._base_url + "edit",
-            files={
-                f"{name}": content
-            },
-            username=username, password=password
-        )
+        self._webservice_client.upload_file(name, content)
 
     def take_screenshot(self) -> bytes:
         """
         Requests a screenshot from the device.
         :return:
         """
-        username = self._device.config.http.user
-        password = self._device.config.http.password
-
-        return self._do_request(
-            method=GET,
-            url=self._base_url + "screenshot",
-            params={
-                "q": "0"
-            },
-            stream=True,
-            username=username, password=password
-        )
-
-    @staticmethod
-    def _do_request(method: str = GET, url: str = "/", params: dict = None,
-                    json: any = None, files: any = None, data: any = None,
-                    headers: dict = None,
-                    username: str = None, password: str = None,
-                    stream: bool = None) -> list or dict or None:
-        """
-        Executes a http request based on the given parameters
-
-        :param method: the method to use (GET, POST)
-        :param url: the url to use
-        :param params: query parameters that will be appended to the url
-        :param json: request body
-        :param headers: custom headers
-        :return: the response parsed as a json
-        """
-        _headers = {
-        }
-        if headers is not None:
-            _headers.update(headers)
-
-        response = requests.request(
-            method, url, headers=_headers,
-            params=params,
-            json=json, files=files,
-            data=data,
-            auth=(username, password),
-            timeout=5,
-            stream=stream,
-        )
-
-        response.raise_for_status()
-        if len(response.content) > 0:
-            if "application/json" in response.headers.get("Content-Type", ""):
-                return response.json()
-            else:
-                return response.content
-
-    @staticmethod
-    def _compute_base_url(device: Device) -> str:
-        url = device.config.openhasp_config_manager.device.ip
-        if not url.startswith("http://"):
-            url = "http://" + url
-        if not url.endswith("/"):
-            url += "/"
-        return url
+        return self._webservice_client.take_screenshot()
---
+++ openhasp_config_manager/openhasp_client/webservice_client.py
@@ -0,0 +1,258 @@
+from typing import Dict, List
+
+import orjson
+import requests
+
+from openhasp_config_manager.openhasp_client.model.gui_config import GuiConfig
+from openhasp_config_manager.openhasp_client.model.hasp_config import HaspConfig
+from openhasp_config_manager.openhasp_client.model.http_config import HttpConfig
+from openhasp_config_manager.openhasp_client.model.mqtt_config import MqttConfig
+
+GET = "GET"
+POST = "POST"
+DELETE = "DELETE"
+
+
+class WebserviceClient:
+
+    def __init__(self, url: str, username: str, password: str):
+        self._username = username
+        self._password = password
+        self._base_url = self._compute_base_url(url)
+
+    def reboot(self):
+        """
+        Request a reboot
+        """
+        self._do_request(
+            method=GET,
+            url=self._base_url + "reboot",
+        )
+
+    def set_hasp_config(self, config: HaspConfig):
+        """
+        Set the "HASP" configuration
+        :param config: the configuration to set
+        """
+        data = {
+            "startpage": config.startpage,
+            "startdim": config.startdim,
+            "theme": config.theme,
+            "color1": config.color1,
+            "color2": config.color2,
+            "font": config.font,
+            "pages": config.pages,
+            "save": "hasp"
+        }
+
+        # ignore keys with None value
+        data = {k: v for k, v in data.items() if v is not None}
+
+        self._do_request(
+            method=POST,
+            url=self._base_url + "config",
+            data=data,
+        )
+
+    def set_http_config(self, config: HttpConfig):
+        """
+        Set the HTTP configuration
+        :param config: the configuration to set
+        """
+        data = {
+            "user": config.user,
+            "pass": config.password,
+            "save": "http"
+        }
+
+        # ignore keys with None value
+        data = {k: v for k, v in data.items() if v is not None}
+
+        self._do_request(
+            method=POST,
+            url=self._base_url + "config",
+            data=data,
+        )
+
+    def get_mqtt_config(self) -> MqttConfig:
+        data = self._do_request(
+            method=GET,
+            url=self._base_url + "api/config/mqtt/",
+        )
+
+        return MqttConfig(
+            name=data["name"],
+            group=data["group"],
+            host=data["host"],
+            port=data["port"],
+            user=data["user"],
+            password=data["pass"]
+        )
+
+    def set_mqtt_config(self, config: MqttConfig):
+        """
+        Set the MQTT configuration
+        :param config: the configuration to set
+        :return:
+        """
+        data = {
+            "name": config.name,
+            "group": config.group,
+            "host": config.host,
+            "port": config.port,
+            "user": config.user,
+            "pass": config.password,
+            "save": "mqtt"
+        }
+
+        # ignore keys with None value
+        data = {k: v for k, v in data.items() if v is not None}
+
+        self._do_request(
+            method=POST,
+            url=self._base_url + "config",
+            data=data,
+        )
+
+    def set_gui_config(self, config: GuiConfig):
+        """
+        Set the GUI configuration
+        :param config: the configuration to set
+        :return:
+        """
+        data = {
+            "idle1": config.idle1,
+            "idle2": config.idle2,
+            "rotate": config.rotate,
+            "cursor": config.cursor,
+            "bckl": config.bckl,
+            "save": "gui"
+        }
+
+        # ignore keys with None value
+        data = {k: v for k, v in data.items() if v is not None}
+
+        self._do_request(
+            method=POST,
+            url=self._base_url + "config",
+            data=data,
+        )
+
+    def upload_files(self, files: Dict[str, str]):
+        """
+        Upload a collection of files
+        :param files: "target file name"->"file content" mapping
+        """
+        for name, content in files.items():
+            self.upload_file(name, content)
+
+    def upload_file(self, name: str, content: str):
+        """
+        Upload a single file
+        :param name: the target name of the file on the device
+        :param content: the file content
+        """
+        self._do_request(
+            method=POST,
+            url=self._base_url + "edit",
+            files={
+                f"{name}": content
+            },
+        )
+
+    def get_files(self) -> List[str]:
+        """
+        Retrieve a list of all file on the device
+        :return: a list of all files on the device
+        """
+        response = self._do_request(
+            method=GET,
+            url=self._base_url + "list?dir=/",
+        )
+        response_data = orjson.loads(response.decode('utf-8'))
+
+        files = list(filter(lambda x: x["type"] == "file", response_data))
+        file_names = list(map(lambda x: x["name"], files))
+        return file_names
+
+    def get_file_content(self, file_name: str) -> str or None:
+        try:
+            response = self._do_request(
+                method=GET,
+                url=self._base_url + file_name,
+            )
+            response_data = response.decode('utf-8')
+            return response_data
+        except Exception as ex:
+            return None
+
+    def delete_file(self, file_name: str):
+        """
+        Delete a file on the device
+        :param file_name: the name of the file
+        """
+        self._do_request(
+            method=DELETE,
+            url=self._base_url + "edit",
+            data={
+                "path": "/" + file_name
+            },
+        )
+
+    def take_screenshot(self) -> bytes:
+        """
+        Requests a screenshot from the device.
+        :return:
+        """
+        return self._do_request(
+            method=GET,
+            url=self._base_url + "screenshot",
+            params={
+                "q": "0"
+            },
+            stream=True,
+        )
+
+    def _do_request(self, method: str = GET, url: str = "/", params: dict = None,
+                    json: any = None, files: any = None, data: any = None,
+                    headers: dict = None,
+                    stream: bool = None) -> list or dict or None:
+        """
+        Executes a http request based on the given parameters
+
+        :param method: the method to use (GET, POST)
+        :param url: the url to use
+        :param params: query parameters that will be appended to the url
+        :param json: request body
+        :param headers: custom headers
+        :return: the response parsed as a json
+        """
+        _headers = {
+        }
+        if headers is not None:
+            _headers.update(headers)
+
+        response = requests.request(
+            method, url, headers=_headers,
+            params=params,
+            json=json, files=files,
+            data=data,
+            auth=(self._username, self._password),
+            timeout=5,
+            stream=stream,
+        )
+
+        response.raise_for_status()
+        if len(response.content) > 0:
+            if "application/json" in response.headers.get("Content-Type", ""):
+                return response.json()
+            else:
+                return response.content
+
+    @staticmethod
+    def _compute_base_url(url: str) -> str:
+        if not url.startswith("http://"):
+            url = "http://" + url
+        if not url.endswith("/"):
+            url += "/"
+        return url
