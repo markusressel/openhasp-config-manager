688f60566cc3b5d762ab921a418be6963e36ddf7
Markus Ressel <mail@markusressel.de>
2023-04-08 21:30:12 +0200

    update example

--- example.py
+++ example.py
@@ -7,6 +7,10 @@ from openhasp_config_manager.processing.variables import VariableManager
 from openhasp_config_manager.uploader import ConfigUploader
 
 
+async def event_callback(topic: str, payload: bytes):
+    print(f"MQTT Event: {topic} - {payload.decode('utf-8')}")
+
+
 async def main():
     config_dir = Path("./openhasp-configs")
     output_dir = Path("./output")
@@ -28,11 +32,18 @@ async def main():
     # deploy the local config to the device
     uploader.upload(device=touch_down_1, purge=False, print_diff=True)
 
+    # subscribe to all MQTT events (async)
+    event_listener_task = client.listen_event(path="#", callback=event_callback)
+    asyncio.create_task(event_listener_task)
+
     # update an object on the device (via MQTT)
     await client.set_text(
         obj="p1b10",
         text="Hello World!",
     )
+
+    # wait forever
+    await asyncio.Event().wait()
 
 
 if __name__ == '__main__':
