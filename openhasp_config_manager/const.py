ab030645edccbde412d6446eb0b7592e50628fe7
Markus Ressel <mail@markusressel.de>
2023-04-10 21:00:53 +0200

    added mqtt_off.cmd and mqtt_on.cmd to SYSTEM_SCRIPTS

--- openhasp_config_manager/const.py
+++ openhasp_config_manager/const.py
@@ -1,4 +1,4 @@
 COMMON_FOLDER_NAME = "common"
 DEVICES_FOLDER_NAME = "devices"
 
-SYSTEM_SCRIPTS = ["boot.cmd", "online.cmd", "offline.cmd"]
+SYSTEM_SCRIPTS = ["boot.cmd", "online.cmd", "offline.cmd", "mqtt_off.cmd", "mqtt_on.cmd"]
