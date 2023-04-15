6048fc228f776eda3823cc4053f23dd75ad6e040
Markus Ressel <mail@markusressel.de>
2023-04-16 01:53:57 +0200

    fix typo

--- openhasp_config_manager/uploader.py
+++ openhasp_config_manager/uploader.py
@@ -114,7 +114,7 @@ class ConfigUploader:
         if new_checksum is not None:
             result = True
             if print_diff:
-                info(f"Binary file '{file.name}' has changed ({device_file_content_checksum} -> {new_checksum}.")
+                info(f"Binary file '{file.name}' has changed ({device_file_content_checksum} -> {new_checksum})")
             try:
                 self._api_client.upload_file(file.name, content)
                 checksum_file = self._get_checksum_file(file)
