6b0e176ef0b1e6e719014ba728948e4cb2a1e168
Markus Ressel <mail@markusressel.de>
2024-03-02 16:48:11 +0100

    migrate to aiomqtt

--- openhasp_config_manager/openhasp_client/openhasp.py
+++ openhasp_config_manager/openhasp_client/openhasp.py
@@ -1,7 +1,7 @@
 import asyncio
 from typing import Dict, List, Any, Callable, Tuple
 
-from asyncio_mqtt import Topic
+from aiomqtt import Topic
 
 from openhasp_config_manager.openhasp_client.image_processor import OpenHaspImageProcessor
 from openhasp_config_manager.openhasp_client.model.device import Device
