4ada70d90bf67c027a41424248a9dae9cf557d4e
Markus Ressel <mail@markusressel.de>
2025-06-13 04:24:50 +0200

    fix qasync

--- openhasp_config_manager/cli/gui.py
+++ openhasp_config_manager/cli/gui.py
@@ -12,10 +12,14 @@ from openhasp_config_manager.ui.qt.main import MainWindow
 async def c_gui(config_dir: Path, output_dir: Path):
     config_manager = _create_config_manager(config_dir, output_dir)
     app = QApplication(sys.argv)
-    loop = QEventLoop(app)
-    asyncio.set_event_loop(loop)
+    event_loop = QEventLoop(app)
+    asyncio.set_event_loop(event_loop)
+
+    app_close_event = asyncio.Event()
+    app.aboutToQuit.connect(app_close_event.set)
 
     window = MainWindow(config_manager)
     window.show()
 
-    app.exec()
+    event_loop.run_until_complete(app_close_event.wait())
+    event_loop.close()
